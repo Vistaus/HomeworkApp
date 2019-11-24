9bcb07c3cd425cfaa9a56799bf33231bc9fcc478
Domi04151309 <domi04151309business@gmail.com>
2019-11-24 18:01:23 +0100

    Add ViewPager

---
+++ app/src/main/java/io/github/domi04151309/homeworkapp/DateAdapter.kt
@@ -0,0 +1,52 @@
+package io.github.domi04151309.homeworkapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.github.domi04151309.homeworkapp.DateAdapter.ViewHolder
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DateAdapter(context: Context, size: Int) : RecyclerView.Adapter<ViewHolder>() {
+
+    private val c: Context = context
+    private val fullSize: Int = size
+    val halfSize: Int = size / 2
+    private var calendar: Calendar = Calendar.getInstance()
+    val saveFormat: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    private val displayFormat: SimpleDateFormat = SimpleDateFormat("d MMM yyyy", Locale.getDefault())
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(c).inflate(R.layout.pager_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        calendar = Calendar.getInstance()
+        calendar.add(Calendar.DATE, position - halfSize)
+        holder.dateTxt.text = displayFormat.format(calendar.time)
+
+        val values = c.resources.getStringArray(R.array.planner_empty)
+        val adapter = ArrayAdapter<String>(
+            c,
+            R.layout.list_item,
+            R.id.text1,
+            values
+        )
+        holder.listView.adapter = adapter
+    }
+
+    override fun getItemCount(): Int {
+        return fullSize
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val dateTxt: TextView = itemView.findViewById(R.id.dateTxt)
+        val listView: ListView = itemView.findViewById(R.id.listView)
+    }
+}
--- app/src/main/java/io/github/domi04151309/homeworkapp/MainActivity.kt
+++ app/src/main/java/io/github/domi04151309/homeworkapp/MainActivity.kt
@@ -2,20 +2,27 @@ package io.github.domi04151309.homeworkapp
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
+import java.util.*
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private var fab: FloatingActionButton? = null
     private var drawerLayout: DrawerLayout? = null
     private var navView: NavigationView? = null
+
+    private var viewPager: ViewPager2? = null
+    private val loadingSize = Integer.MAX_VALUE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Theme.setNoActionBar(this)
@@ -26,16 +33,26 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawerLayout = findViewById(R.id.drawer_layout)
         navView = findViewById(R.id.nav_view)
 
-        fab!!.setOnClickListener {
-            //startActivity(Intent(this, AddActivity::class.java))
-        }
-
         findViewById<ImageView>(R.id.menu_icon).setOnClickListener {
             drawerLayout!!.openDrawer(GravityCompat.START)
         }
 
         navView!!.setNavigationItemSelectedListener(this)
         navView!!.setCheckedItem(R.id.nav_planner)
+
+       viewPager = findViewById(R.id.pager)
+        val dateAdapter = DateAdapter(this, loadingSize)
+
+        viewPager!!.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        viewPager!!.adapter = dateAdapter
+        viewPager!!.currentItem = loadingSize / 2
+
+        fab!!.setOnClickListener {
+            val calendar: Calendar = Calendar.getInstance()
+            calendar.add(Calendar.DATE, viewPager!!.currentItem - dateAdapter.halfSize)
+            Toast.makeText(this, dateAdapter.saveFormat.format(calendar.time), Toast.LENGTH_LONG).show()
+            //startActivity(Intent(this, AddActivity::class.java))
+        }
     }
 
     override fun onBackPressed() {
@@ -47,6 +64,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
+            R.id.nav_planner -> {
+                viewPager!!.currentItem = loadingSize / 2
+            }
             R.id.nav_settings -> {
                 startActivity(Intent(this, Preferences::class.java))
             }
