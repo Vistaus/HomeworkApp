bcebb9109a25804b3f8052c7edb5bc8ae87df360
Domi04151309 <domi04151309business@gmail.com>
2020-04-20 16:39:40 +0200

    Update AboutActivity

--- app/src/main/java/io/github/domi04151309/homeworkapp/AboutActivity.kt
+++ app/src/main/java/io/github/domi04151309/homeworkapp/AboutActivity.kt
@@ -2,6 +2,7 @@ package io.github.domi04151309.homeworkapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import io.github.domi04151309.homeworkapp.objects.Theme
 
@@ -12,5 +13,8 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
         findViewById<TextView>(R.id.versionTxt).text = resources.getString(R.string.about_version, BuildConfig.VERSION_NAME)
+        findViewById<TextView>(R.id.github).movementMethod = LinkMovementMethod.getInstance()
+        findViewById<TextView>(R.id.license).movementMethod = LinkMovementMethod.getInstance()
+        findViewById<TextView>(R.id.icons).movementMethod = LinkMovementMethod.getInstance()
     }
 }
