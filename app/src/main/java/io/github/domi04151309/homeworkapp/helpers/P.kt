2b7df7c26f4bec6b46198876d8da08e4bbe3f905
Domi04151309 <domi04151309business@gmail.com>
2020-11-07 16:25:14 +0100

    Add system theme option

--- app/src/main/java/io/github/domi04151309/homeworkapp/helpers/P.kt
+++ app/src/main/java/io/github/domi04151309/homeworkapp/helpers/P.kt
@@ -2,5 +2,5 @@ package io.github.domi04151309.homeworkapp.helpers
 
 internal object P {
     const val PREF_THEME = "theme"
-    const val PREF_THEME_DEFAULT = "light"
+    const val PREF_THEME_DEFAULT = "auto"
 }
--- app/src/main/java/io/github/domi04151309/homeworkapp/helpers/Theme.kt
+++ app/src/main/java/io/github/domi04151309/homeworkapp/helpers/Theme.kt
@@ -3,42 +3,44 @@ package io.github.domi04151309.homeworkapp.helpers
 import android.app.Activity
 import android.app.ActivityManager
 import android.content.Context
+import android.content.res.Configuration
 import android.graphics.BitmapFactory
 import androidx.preference.PreferenceManager
 import androidx.core.content.ContextCompat
 import io.github.domi04151309.homeworkapp.R
 
-object Theme {
+internal object Theme {
 
     fun set(context: Context) {
-        when (PreferenceManager.getDefaultSharedPreferences(context).getString("theme", "light")) {
+        when (PreferenceManager.getDefaultSharedPreferences(context)
+            .getString(P.PREF_THEME, P.PREF_THEME_DEFAULT)) {
             "light" -> {
                 context.setTheme(R.style.AppTheme27)
-                recent(
-                    context,
-                    R.color.colorPrimary
-                )
+                recent(context, R.color.colorPrimary)
             }
             "dark" -> {
                 context.setTheme(R.style.AppThemeDark)
-                recent(
-                    context,
-                    R.color.colorPrimaryDark
-                )
+                recent(context, R.color.colorPrimaryDark)
             }
             "black" -> {
                 context.setTheme(R.style.AppThemeBlack)
-                recent(
-                    context,
-                    R.color.colorPrimaryBlack
-                )
+                recent(context, R.color.colorPrimaryBlack)
+            }
+            "auto" -> {
+                when (context.resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        context.setTheme(R.style.AppThemeDark)
+                        recent(context, R.color.colorPrimaryDark)
+                    }
+                    else -> {
+                        context.setTheme(R.style.AppTheme27)
+                        recent(context, R.color.colorPrimary)
+                    }
+                }
             }
             else -> {
                 context.setTheme(R.style.AppTheme27)
-                recent(
-                    context,
-                    R.color.colorPrimary
-                )
+                recent(context, R.color.colorPrimary)
             }
         }
         context.setTheme(R.style.AppThemePatch)
@@ -48,31 +50,31 @@ object Theme {
         when (PreferenceManager.getDefaultSharedPreferences(context).getString("theme", "light")) {
             "light" -> {
                 context.setTheme(R.style.AppTheme_NoActionBar)
-                recent(
-                    context,
-                    R.color.colorPrimary
-                )
+                recent(context, R.color.colorPrimary)
             }
             "dark" -> {
                 context.setTheme(R.style.AppThemeDark_NoActionBar)
-                recent(
-                    context,
-                    R.color.colorPrimaryDark
-                )
+                recent(context, R.color.colorPrimaryDark)
             }
             "black" -> {
                 context.setTheme(R.style.AppThemeBlack_NoActionBar)
-                recent(
-                    context,
-                    R.color.colorPrimaryBlack
-                )
+                recent(context, R.color.colorPrimaryBlack)
+            }
+            "auto" -> {
+                when (context.resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        context.setTheme(R.style.AppThemeDark_NoActionBar)
+                        recent(context, R.color.colorPrimaryDark)
+                    }
+                    else -> {
+                        context.setTheme(R.style.AppTheme_NoActionBar)
+                        recent(context, R.color.colorPrimary)
+                    }
+                }
             }
             else -> {
                 context.setTheme(R.style.AppTheme_NoActionBar)
-                recent(
-                    context,
-                    R.color.colorPrimary
-                )
+                recent(context, R.color.colorPrimary)
             }
         }
         context.setTheme(R.style.AppThemePatch)
@@ -81,10 +83,7 @@ object Theme {
     private fun recent(c: Context, color: Int) {
         val taskDescription = ActivityManager.TaskDescription(
             c.getString(R.string.app_name),
-            BitmapFactory.decodeResource(
-                c.resources,
-                R.mipmap.ic_launcher
-            ),
+            BitmapFactory.decodeResource(c.resources, R.mipmap.ic_launcher),
             ContextCompat.getColor(c, color)
         )
         (c as Activity).setTaskDescription(taskDescription)
